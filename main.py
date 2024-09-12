80e9dc20410100195853c44f332df409212f00fd
mordechaisav <mordechy7@gmail.com>
2024-09-12 10:41:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+import csv
+def read_from_json(path):
+    with open(path, 'r') as file:
+        json_data = file.read()
+    data = json.loads(json_data)
+    return data
+
+
+
+
+
+
+
+
